0c654f42895caa00edb8e9d00a4d9d2f1d34518d
Isadora Nepomuceno <iisadoranepomuceno@gmail.com>
2023-04-08 15:06:11 -0300

    Obtendo um post por id

---
+++ workshopmongo/src/main/java/com/isadoranepomuceno/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.isadoranepomuceno.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isadoranepomuceno.workshopmongo.domain.Post;
+import com.isadoranepomuceno.workshopmongo.repository.PostRepository;
+import com.isadoranepomuceno.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
